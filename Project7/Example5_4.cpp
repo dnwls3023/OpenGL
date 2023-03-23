d86843c013dd4b1cfba38d040341de031c17f26d
hanw00jin <dnwls3023@hs.ac.kr>
2023-03-23 21:10:58 +0900

    예제5-4

---
+++ Project7/Example5_4.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+
+void MyDisplay() { // 디스플레이 콜백 함수
+	glClear(GL_COLOR_BUFFER_BIT); // GL 상태 변수 설정
+	glViewport(0, 0, 300, 300);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("OpenGL Sample Drawing");
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
+}
